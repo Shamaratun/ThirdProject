5472b7c568eedd71863f1638c169d8f351c50d24
Meme <shamaratunmeme@gmail.com>
2025-02-20 00:22:20 +0600

    product form complete,table need correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,24 @@ import{ContentComponent}from './content/content.component';
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent,FooterComponent, ContentComponent],
-  // templateUrl: './app.component.html',
-  template:`
-   <div class ="container">
-  <app-header></app-header >
-  <app-content></app-content>
-  <app-footer></app-footer>
+  templateUrl: './app.component.html',
+//   template:`
+//    <div class ="container">
+//   <app-header></app-header >
+//   <app-content></app-content>
+//   <app-footer></app-footer>
 
-</div>
-`,
+// </div>
+// `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+Meme: any;
+deleteUser(_t17: any) {
+throw new Error('Method not implemented.');
+}
+editUser(_t17: any) {
+throw new Error('Method not implemented.');
+}
   title = 'ThirdProject';
 }
